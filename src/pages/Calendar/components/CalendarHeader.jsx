54ecaad51fc763feb09916b0efb765a85c642357
namuleaf <namuleaf2@gmail.com>
2026-07-15 18:58:32 +0900

    feat: 일정 캘린더/종목 필터링 구현, docs: MatchCard 원상태 복귀

---
+++ src/pages/Calendar/components/CalendarHeader.jsx
@@ -0,0 +1,23 @@
+﻿import css from "../Calendar.module.css";
+import { IoIosArrowBack } from "react-icons/io"
+import { IoIosArrowForward } from "react-icons/io";
+
+const CalendarHeader = ({ year, month, onPrevMonth, onNextMonth }) => {
+  return (
+    <header className={css.calendarHeader}>
+      <button type="button" onClick={onPrevMonth} className={css.navButton} aria-label="이전 달">
+        <IoIosArrowBack />
+      </button>
+
+      <h1 className={css.calendarTitle}>
+        {year}년 {month + 1}월
+      </h1>
+
+      <button type="button" onClick={onNextMonth} className={css.navButton} aria-label="다음 달">
+        <IoIosArrowForward />
+      </button>
+    </header>
+  );
+};
+
+export default CalendarHeader;
