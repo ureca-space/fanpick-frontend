2d5e62699e93aea4e6ae2a64d3e6c6abd42b87fd
HaeJun <haejunbag131@gmail.com>
2026-07-16 10:48:34 +0900

    feat: 헤더 메뉴 개편 및 신규 페이지 라우팅 추가

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
-import MyPage from "../pages/MyPage/MyPage";
-import SportPage from "../pages/Sport/SportPage";
+
 import HomePage from "../pages/Home/HomePage";
+import PlayersPage from "../pages/Players/PlayersPage";
+import MatchSchedulePage from "../pages/MatchSchedule/MatchSchedulePage";
+import CalendarPage from "../pages/Calendar/CalendarPage";
 import PredictionPage from "../pages/Prediction/PredictionPage";
 import WorldCupPage from "../pages/WorldCup/WorldCupPage";
 import WorldCupPlayPage from "../pages/WorldCupPlay/WorldCupPlayPage";
+import MyPage from "../pages/MyPage/MyPage";
 import NotFoundPage from "../pages/NotFound/NotFoundPage";
+
 import LoginPage from "../pages/Auth/Login/LoginPage";
 import SignupPage from "../pages/Auth/Signup/SignupPage";
 import FindPasswordPage from "../pages/Auth/FindPassword/FindPasswordPage";
-import ResetPasswordPage from "../pages/Auth/ResetPassword/ResetPassword.jsx";
+import ResetPasswordPage from "../pages/Auth/ResetPassword/ResetPassword";
+
 import ProtectedRoute from "./ProtectedRoute";
 import ScrollToTop from "./ScrollToTop";
 
@@ -23,13 +28,16 @@ const AppRouter = () => {
         <Route element={<MainLayout />}>
           {/* 누구나 접근 가능 */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/sports/:sport" element={<SportPage />} />
+
+          <Route path="/players" element={<PlayersPage />} />
+          <Route path="/matches" element={<MatchSchedulePage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+
           <Route path="/worldcup" element={<WorldCupPage />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-
           <Route path="/find-password" element={<FindPasswordPage />} />
-
           <Route path="/reset-password" element={<ResetPasswordPage />} />
 
           {/* 로그인 사용자만 접근 가능 */}
@@ -39,6 +47,7 @@ const AppRouter = () => {
             <Route path="/mypage" element={<MyPage />} />
           </Route>
 
+          {/* 존재하지 않는 경로 */}
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
